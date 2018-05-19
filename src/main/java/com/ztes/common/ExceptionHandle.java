e994de73d958c606c7ee89ff5e67ce31836f0875
muling105 <muling105@aliyun.com>
2018-05-19 15:38:35 +0800

    日志和lombok插件

--- src/main/java/com/ztes/common/ExceptionHandle.java
+++ src/main/java/com/ztes/common/ExceptionHandle.java
@@ -2,6 +2,7 @@ package com.ztes.common;
 
 import com.ztes.common.enums.ResultEnum;
 import com.ztes.common.exception.UserException;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 自定义捕获异常
  */
 @ControllerAdvice
+@Slf4j
 public class ExceptionHandle {
 
-    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
+    //private static final Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
 
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
@@ -23,7 +25,7 @@ public class ExceptionHandle {
             UserException userException = (UserException)e;
             return ResultUtil.error(userException.getCode(), userException.getMessage());
         }else {
-            logger.error("系统错误：{}", e);
+            log.error("系统错误：{}", e);
             return ResultUtil.error(ResultEnum.UNKONW_ERROR.getCode(), ResultEnum.UNKONW_ERROR.getMsg());
         }
 
